56b4a4ba0260265c9f7f2e62a8b89c0915a74109
shauryachr <shauryachr@gmail.com>
2026-06-18 11:22:50 +0530

    Delete core directory

--- core/track_source.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @public
- *
- * The element to be used as source for the handtracking. Usually this is a `<video>` element.
- */
-export type ITrackSource = HTMLCanvasElement|HTMLVideoElement|HTMLImageElement|OffscreenCanvas
